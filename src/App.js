00f4f7be5385a292d94bddf8bc48886a8050e16d
Duong Dinh <ddinh11@hotmail.com>
2020-03-22 13:19:33 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Album from './Album'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Album />
+      </header>
+    </div>
+  );
+}
+
+export default App;
